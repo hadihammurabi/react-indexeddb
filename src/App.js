463d7fec55d48392428765d04e4cff3cdff27a44
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2018-08-14 07:33:35 +0700

    Membuat list component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import List from './components/List';
+import { Container, Row } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Form/>
-      </div>
+      <Container>
+        <Row>
+          <Form/>
+        </Row>
+        <Row>
+          <List/>
+        </Row>
+      </Container>
     );
   }
 }
